e8511dd38a74441d1ef30961f610460d5e687344
Fictional <fictionalaroma@gmail.com>
2023-05-21 15:31:04 +0100

    Basic Player Movement

--- unity-project/Assets/Scripts/Player/PlayerController.cs
+++ unity-project/Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,67 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Helpers;
+using UnityEngine.Serialization;
 
-namespace Assets.Scripts.Player
+namespace Player
 {
 	public class PlayerController : MonoBehaviour, PlayerInput.IPlayerActions
 	{
+		private PlayerInput _controls;
+		private Vector2 _currentMoveInputVector = Vector2.zero;
+		private InputAction _moveAction;
+        private CharacterController _characterController;
+
+		[SerializeField] private float moveSpeed;
+		private float _playerGrav;
+		[SerializeField] private float gravityValue;
+
 		// Start is called before the first frame update
-		void Start()
+		void Awake()
 		{
-        
+			GetComponent<Collider>();
+			GetComponent<Rigidbody>();
+			_characterController = GetComponent<CharacterController>();
+
+			_controls = new PlayerInput();
+			_moveAction = _controls.Player.Move;
+
+			_moveAction.performed += OnMove;
 		}
 
-		// Update is called once per frame
-		void Update()
+		private void OnEnable()
 		{
-        
+			EnableControls();
 		}
 
-		public void OnMove(InputAction.CallbackContext context) { throw new System.NotImplementedException(); }
+		private void EnableControls()
+		{
+			_controls.Enable();
+			_controls.Player.Enable();
+		}
 
-		public void OnLook(InputAction.CallbackContext context) { throw new System.NotImplementedException(); }
+
+        private void Update()
+		{
+			var temp = _currentMoveInputVector.ToVector3TopDown() * (Time.deltaTime * moveSpeed);
+
+			_playerGrav = _characterController.isGrounded ? 0f : gravityValue * Time.deltaTime;
+
+			temp.y = _playerGrav;
+
+            _characterController.Move(temp);
+
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+		{
+			_currentMoveInputVector = !context.canceled ? context.ReadValue<Vector2>() : Vector2.zero;
+
+        }
+
+        public void OnLook(InputAction.CallbackContext context) { throw new System.NotImplementedException(); }
 
 		public void OnFire(InputAction.CallbackContext context) { throw new System.NotImplementedException(); }
+		public void OnDodge(InputAction.CallbackContext context) { throw new System.NotImplementedException(); }
 	}
 }
