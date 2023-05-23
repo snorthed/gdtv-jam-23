23a531841218e4519bb2ddd895c9aa8a19f93fc3
Olddevbless <edevilbless@gmail.com>
2023-05-23 15:50:14 +0300

    primaryMelee attack

--- unity-project/Assets/Scripts/Player/Weapons/Melee.cs
+++ unity-project/Assets/Scripts/Player/Weapons/Melee.cs
@@ -1,17 +1,34 @@
 using CommonComponents.Interfaces;
+using System.Collections;
 using UnityEngine;
 
 namespace Player.Weapons
 {
 	public class Melee : BaseWeapon  
 	{
-    
+		private Coroutine _punching;
 
 		public override void BeginPrimaryAttack(Vector3 fireDirection)
 		{
-			throw new System.NotImplementedException();
+			Debug.Log("Start Firing");
+			FireDirection = fireDirection;
+			_punching = StartCoroutine(PunchingRepeater());
 		}
 
+		private IEnumerator PunchingRepeater()
+		{
+			while (true)
+			{
+				Collider[] _enemyColliders = Physics.OverlapSphere(weaponsSetup._PrimaryProjectile.transform.position, weaponsSetup._PrimaryRange);
+				for (int i =0; i<_enemyColliders.Length; i++)
+                {
+					//enemyTakeDamage or Send hit?
+                }
+				
+				yield return new WaitForSeconds(weaponsSetup._PrimaryCooldown);
+			}
+		}
+		
 		public override void SecondaryAttack(Vector3 fireDirection)
 		{
 			throw new System.NotImplementedException();
